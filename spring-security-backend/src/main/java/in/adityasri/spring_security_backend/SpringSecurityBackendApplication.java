ab32c0b2e44fda3385f5a457489997c5d5ee9cdd
adityasrivastava29 <adityasrivastava.niet@gmail.com>
2026-09-25 12:44:05 +0530

    springboot  backend initial commit

---
+++ spring-security-backend/src/main/java/in/adityasri/spring_security_backend/SpringSecurityBackendApplication.java
@@ -0,0 +1,13 @@
+package in.adityasri.spring_security_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBackendApplication.class, args);
+	}
+
+}
---
+++ spring-security-backend/src/test/java/in/adityasri/spring_security_backend/SpringSecurityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package in.adityasri.spring_security_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
